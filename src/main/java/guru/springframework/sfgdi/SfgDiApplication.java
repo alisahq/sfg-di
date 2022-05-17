b022ec3fd7793c915ea9ef1078c790ae6ac7097f
alisahq <alisahq@umich.edu>
2022-05-17 15:16:24 -0400

    updated the spring context

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		// .run will return an application context
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		// the getBean method should get the lower case of the controller's class name
+		// through the context getBean method, spring constructs a controller object for us.
+		MyController myController = (MyController)ctx.getBean("myController");
+		// get the return value of the sayHello method in the controller and store it to the string "greeting"
+		String greeting = myController.sayHello();
+		// sayHello method is also evoked. so the print in sayHello is also present in the console.
+		System.out.println(greeting);
+	}
+
+}
